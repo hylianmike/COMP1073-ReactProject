6859a3159a7e79f431ebd772108c4def0d3cd97c
JackDouma <97615212+JackDouma@users.noreply.github.com>
2022-02-26 13:38:53 -0500

    Update App.js
    added piechart to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Chart as Chartjs } from 'chart.js/auto';
 import BarChart from './components/Bar'
+import PieChart from './components/Pie'
 
 function App() {
 return (
 	<div className="App">
-        <BarChart />
+        	<BarChart />
+		<PieChart />
 	</div>
 );
 }
